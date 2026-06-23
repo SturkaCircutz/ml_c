97e8963783752092076665773d36bbaad2e7efd9
jiawen <313721325sjw@gmail.com>
2026-06-23 10:12:42 +0800

    add bias, almost finished the or gates.

--- gates.c
+++ gates.c
@@ -15,12 +15,12 @@ float train[][3] = {
 };
 #define train_count (sizeof(train)/sizeof(train[0]))
 
-float cost(float w1, float w2){
+float cost(float w1, float w2, float b1){
   float result = 0.0f;
   for(size_t i = 0; i < train_count; i++){
     float x1= train[i][0];
     float x2= train[i][1];
-    float y = sigmoidf(x1 * w1 + x2*w2);
+    float y = sigmoidf(x1 * w1 + x2*w2+b1);
     float d = y - train[i][2];
     result += d * d;
   }
@@ -32,24 +32,29 @@ float cost(float w1, float w2){
 float rand_float(void){
   return (float) rand()/(float) RAND_MAX;
 }
-
+ 
 int main(){
-  for(float x = -10.f; x<=10; x+=1.f){
-    printf("%f => %f\n", x, sigmoidf(x));
-  }
   srand(time(0));
-  float eps = 10e-3;
-  float rate = 10e-3;
-  float w1 = rand_float(); // weight
-  float w2 = rand_float();
-  for(size_t i = 0; i<500; ++i){
-    printf("cost = %f, w1 = %f, w2 = %f\n", cost(w1, w2), w1, w2);
-    float dcost1 = (cost(w1+eps, w2) - cost(w1, w2))/eps;
-    float dcost2 = (cost(w1, w2+eps) - cost(w1, w2))/eps; 
+  float eps = 1e-2;
+  float rate = 1e-1;
+  float w1 = rand_float()*10 - 5; // weight
+  float w2 = rand_float()*10 - 5;
+  float b1 = rand_float()*10;
+  for(size_t i = 0; i<1000;  ++i){
+    printf("cost = %f, w1 = %f, w2 = %f\n", cost(w1, w2, b1), w1, w2);
+    float dcost1 = (cost(w1+eps, w2, b1) - cost(w1, w2, b1))/eps;
+    float dcost2 = (cost(w1, w2+eps, b1) - cost(w1, w2, b1))/eps;
+    float db = (cost(w1, w2, b1+eps) - cost(w1, w2, b1))/eps;
     w1 -= rate * dcost1;
     w2 -= rate * dcost2;
-     
+    b1 -= rate * db;
   }
+
+   for(size_t i = 0; i < 2; ++i){
+      for(size_t j = 0; j < 2; ++j){
+	printf("%zu | %zu = %f\n", i, j, sigmoidf(i*w1 + j*w2+b1));
+      }
+    }
+     
   return 0;
    }
- 

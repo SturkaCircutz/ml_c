0ba6f9ddd59fdbd48353357595149fb5f4d631b1
SturkaCircutz <313721325sjw@gmail.com>
2026-07-06 11:03:51 +0800

    finish the implementation and debugging for the adder.c

--- nn/adder.c
+++ nn/adder.c
@@ -16,25 +16,38 @@ for(size_t i = 0; i < in.rows; i++){
     for(size_t j = 0; j < BITS; j++){
 	CAL_MAT(in, i, j) = (x>>j)&1;
 	CAL_MAT(in, i, j + BITS) = (y>>j)&1;
-	if(overflow){
-	    CAL_MAT(out, i, j) = 0;
-	}else{
-	    CAL_MAT(out, i, j) = (z>>j)&1;
-	}
-	    CAL_MAT(out, i, BITS) = overflow;
+	CAL_MAT(out, i, j) = (z>>j)&1;
+	CAL_MAT(out, i, BITS) = overflow;
     }
 }
 MAT_PRINT(in);
 MAT_PRINT(out);
-float rate = 1e-1;
-size_t  layer_struct[] = {2*BITS, BITS+1, BITS+1};
-
+float rate = 1;
+size_t  layer_struct[] = {2*BITS, 2*BITS, BITS+1};
 NN nn = NN_config(LIST_COUNT(layer_struct), layer_struct);
 NN g = NN_config(LIST_COUNT(layer_struct), layer_struct);
-for(size_t i = 0; i < 1000; ++i){
+nn_rand(nn,0,1);
+for(size_t i = 0; i < 1000*10; ++i){
     nn_backprop(nn, g, in, out);
     nn_learn(nn, g, rate);
     printf("cost = %f\n", nn_cost(nn, in, out));    
 }
-
+for(size_t i = 0; i < n; ++i){
+    for(size_t j = 0; j < n; ++j){
+	printf("%zu + %zu =", i, j);
+	for(size_t k = 0; k < BITS; ++k){
+	    CAL_MAT(NN_INPUT(nn), 0, j) = (i>>k)&1;
+	    CAL_MAT(NN_INPUT(nn), 0, j+BITS) = (j>>k)&1;
+	}
+	nn_forward(nn);
+	if(CAL_MAT(NN_OUTPUT(nn), 0, BITS) > 0.5f) printf("overflow\n");
+	else{size_t z = 0;
+	for(size_t m = 0; m < BITS; ++m){
+	    size_t b = CAL_MAT(NN_OUTPUT(nn), 0, m) > 0.5f;
+	    z |= b<<m;		
+	}
+	printf("%zu\n", z);
+    }
+}
+}
 }
